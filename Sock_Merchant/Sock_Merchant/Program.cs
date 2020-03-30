eb70bd540923c2f9b68d44321c91cb53f4f8cdb5
$udiSimba <sudisimbadav@gmail.com>
2020-03-30 15:59:48 +0200

    Solution for Sock Merchant in C#

---
+++ Sock_Merchant/Sock_Merchant/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Sock_Merchant
+{
+    class Program
+    {
+        // Complete the sockMerchant function below.
+        // added this below!
+        static int sockMerchant(int n, int[] ar)
+        {
+            var socks = ar.ToList().GroupBy(x => x);
+            var totPairOfSocks = 0;
+            foreach (var item in socks)
+            {
+                totPairOfSocks += item.Count() / 2;
+            }
+            return totPairOfSocks;
+        }
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+            int result = sockMerchant(n, ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
