ddea58cd4d56570c30b8fd0d1e4a2a440c0df15f
$udiSimba <sudisimbadav@gmail.com>
2020-03-30 16:02:35 +0200

    Solution for Counting Valleys in C#

---
+++ Counting_Valleys/Counting_Valleys/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Counting_Valleys
+{
+    class Program
+    {
+        // Complete the countingValleys function below.
+        static int countingValleys(int n, string s)
+        {
+            int level = 0;
+            int valleyCount = 0;
+            Array numberOfSteps = s.ToArray();
+            foreach (char step in numberOfSteps)
+            {
+                if (step == 'D')
+                    level--;
+                if (step == 'U')
+                {
+                    level++;
+                    if (level == 0)
+                        valleyCount++;
+                }
+            }
+            return valleyCount;
+        }
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string s = Console.ReadLine();
+
+            int result = countingValleys(n, s);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+ }
+
